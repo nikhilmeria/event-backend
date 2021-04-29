aa9acb31110a37a76127f289812de36eacef95e1
nikhil <nrmeria@gmail.com>
2021-04-29 16:00:05 +0530

    1st push after add event functionality in frontend

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e888ac844471d3eec5814209c8c92d84'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4a9da762-2606-4588-ae67-ab37cfb93f08'
+};
